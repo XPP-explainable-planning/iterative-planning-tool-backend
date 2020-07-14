66ec44d13d4c9256389f7a610be96e5a5858a78c
Rebecca Eifler <eifler@cs.uni-saarland.de>
2020-07-14 14:40:00 +0200

    fix undefined settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,12 @@
 
-export const experimentsRootPath = process.env.EXPERIMENTROOTPATH;
-export const planner = process.env.PLANNER;
-export const propertyChekcer = process.env.PROPERTYCHECKER;
-export const demoGenerator = process.env.DEMOGENERATOR;
+export const experimentsRootPath = process.env.EXPERIMENTROOTPATH ? process.env.EXPERIMENTROOTPATH : '';
+export const planner = process.env.PLANNER ? process.env.PLANNER : '';
+export const propertyChekcer = process.env.PROPERTYCHECKER ? process.env.PROPERTYCHECKER : '';
+export const demoGenerator = process.env.DEMOGENERATOR ? process.env.DEMOGENERATOR : '';
 
-export const uploadsPath =  process.env.UPLOADPATH;
-export const resultsPath = process.env.RESULTPATH;
-export const serverResultsPath = process.env.SERVERRESULTPATH;
+export const uploadsPath =  process.env.UPLOADPATH ? process.env.UPLOADPATH : '';
+export const resultsPath = process.env.RESULTPATH ? process.env.RESULTPATH : '';
+export const serverResultsPath = process.env.SERVERRESULTPATH ? process.env.SERVERRESULTPATH : '';
 
-export const ltlkit = process.env.LTLFKIT;
-export const spot = process.env.SPOT;
+export const ltlkit = process.env.LTLFKIT ? process.env.LTLFKIT : '';
+export const spot = process.env.SPOT  ? process.env.SPOT : '';
