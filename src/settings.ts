90f8a551937108eb7f8bfed54801aed6c3f2e131
Rebecca Eifler <eifler@cs.uni-saarland.de>
2020-07-14 13:42:25 +0200

    update env

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,12 @@
 
-export const experimentsRootPath = '/home/eifler/XPP/test';
-export const planner = '/home/eifler/XPP/framework/fast-downward/';
-export const propertyChekcer = '/home/eifler/XPP/framework/property_plan_checker/';
-export const demoGenerator = '/home/eifler/XPP/framework/demo_generator/';
+export const experimentsRootPath = process.env.EXPERIMENTROOTPATH;
+export const planner = process.env.PLANNER;
+export const propertyChekcer = process.env.PROPERTYCHECKER;
+export const demoGenerator = process.env.DEMOGENERATOR;
 
-export const uploadsPath = '/home/eifler/XPP/programms/EXPLORE_backend_ts/dist/out-tsc/uploads';
-export const resultsPath = '/home/eifler/XPP/programms/EXPLORE_backend_ts/dist/out-tsc/results';
-export const serverResultsPath = 'http://localhost:3000/results';
+export const uploadsPath =  process.env.UPLOADPATH;
+export const resultsPath = process.env.RESULTPATH;
+export const serverResultsPath = process.env.SERVERRESULTPATH;
 
-export const ltlkit = '/home/eifler/XPP/framework/ltlfkit/';
-export const spot = '/home/eifler/XPP/programms/Spot/spot-2.6.3/bin/';
+export const ltlkit = process.env.LTLFKIT;
+export const spot = process.env.SPOT;
